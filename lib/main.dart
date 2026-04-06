f1e7f9f3c86900202747653afbbe63c01b31c604
KabilanAndroid <kabilan@evapps.in>
2026-04-06 12:31:18 +0530

    basic

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+
+}
